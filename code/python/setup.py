837b637dc3cb2c5233f34bf3b90004b501859bb3
weiju <weiju.wu@gmail.com>
2023-04-27 10:46:15 -0700

    update to version 0.2.5

--- code/python/setup.py
+++ code/python/setup.py
@@ -9,7 +9,7 @@ DESCRIPTION = 'globalsearch is a collection of Python modules and command tools 
 LICENSE = 'LGPL V3'
 URI = 'https://github.com/baliga-lab/Global_Search'
 AUTHOR = 'Wei-ju Wu'
-VERSION = '0.2.4'
+VERSION = '0.2.5'
 
 KEYWORDS = ['global search', 'coral reef']
 
